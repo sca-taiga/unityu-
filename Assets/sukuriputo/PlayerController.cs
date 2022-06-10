88e3cce266fddb077808d1c5c4eccab752f0d8eb
sca-taiga <104498281+sca-taiga@users.noreply.github.com>
2022-06-10 21:25:17 +0900

    Add files via upload

---
+++ Assets/sukuriputo/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 20; // 動く速さ
+
+    private Rigidbody rb; // Rididbody
+
+    void Start()
+    {
+        // Rigidbody を取得
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // カーソルキーの入力を取得
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        // カーソルキーの入力に合わせて移動方向を設定
+        var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        // Ridigbody に力を与えて玉を動かす
+        rb.AddForce(movement * speed);
+    }
+}
